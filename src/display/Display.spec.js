09a238549f4588291ccd9889fcfa8453c2d1c30d
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2019-12-04 23:05:45 -0800

    Added test for classes to toggle with toggling the buttons

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -12,3 +12,43 @@ test ("<Display /> snapshop", () =>  {
     expect(wrapper.asFragment()).toMatchSnapshot();
 
 }); 
+
+test('Use the red-led class when Locked or Closed', () => {
+    let props = {
+        closed: true,
+        locked: true,
+    };
+
+    const { container } = rtl.render(
+        <Display closed={props.closed} locked={props.locked} />,
+    );
+
+    const buttons = container.querySelectorAll('.led.red-led');
+    
+    const locked = buttons[0];
+    const closed = buttons[1];
+    
+    expect(locked.textContent).toBe('Locked');
+    expect(closed.textContent).toBe('Closed');
+
+});
+
+test('Use the green-led class when Unlocked or Open', () => {
+    let props = {
+        closed: false,
+        locked: false,
+    };
+
+    const { container } = rtl.render(
+        <Display closed={props.closed} locked={props.locked} />,
+    );
+
+    const buttons = container.querySelectorAll('.led.green-led');
+    
+    const locked = buttons[0];
+    const closed = buttons[1];
+    
+    expect(locked.textContent).toBe('Unlocked');
+    expect(closed.textContent).toBe('Open');
+
+});

3a0667072b60062e3439806611463977b2711887
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2019-12-04 22:54:16 -0800

    Added basic render test for each component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,8 +1,14 @@
-// Test away!
+// Test away
 import React from 'react';
-import renderer from 'react-test-renderer';
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import Controls from './Controls';
 
+test ("<Controls /> snapshop", () =>  {
+
+    const wrapper = rtl.render(<Controls />);
+
+    expect(wrapper.asFragment()).toMatchSnapshot();
+
+}); 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+// Test away
+import React from 'react';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Display from './Display';
+
+test ("<Display /> snapshop", () =>  {
+
+    const wrapper = rtl.render(<Display />);
+
+    expect(wrapper.asFragment()).toMatchSnapshot();
+
+}); 
